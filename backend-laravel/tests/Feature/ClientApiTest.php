486033bafd297f872457e7908e3154eaaf0e93be
Marlon Meneses <marlon-armando@hotmail.com>
2025-12-02 14:58:03 -0500

    More tests added to ClientApiTest

--- backend-laravel/tests/Feature/ClientApiTest.php
+++ backend-laravel/tests/Feature/ClientApiTest.php
@@ -44,6 +44,36 @@ class ClientApiTest extends TestCase
         $this->assertCount(5, $response->json('data'));
     }
 
+    public function testIndexWithFilterAndPageNumber(): void
+    {
+        $client = Client::factory()->create(
+            [
+                'type_of_identity_document_id' => TypeOfIdentityDocument::factory()->create()->id,
+                'area_id' => Area::factory()->create()->id
+            ]
+        );
+
+        $page_number = 1;
+
+        $response = $this->getJson("/api/clients?page={$page_number}&identity_document={$client->identity_document}");
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure(
+            [
+                'data' => [
+                    '*' => [
+                        'id',
+                        'first_last_name',
+                        'second_last_name',
+                        'first_name',
+                        'other_names',
+                    ]
+                ]
+            ]
+        );
+    }
+
     public function testStore(): void
     {
         $type_of_identity_document = TypeOfIdentityDocument::factory()->create();
@@ -102,6 +132,42 @@ class ClientApiTest extends TestCase
                 ]
             ]
         );
+    }
+
+    public function testStoreWithWrongFormat(): void
+    {
+        $type_of_identity_document = TypeOfIdentityDocument::factory()->create();
+        $area = Area::factory()->create();
+
+        $currentDateString = date('Y-m-d H:i:s');
+        
+        $body = [
+            'identity_document' => "16450360",
+            'first_last_name' => "meneses",
+            'second_last_name' => "BEJARANO",
+            'first_name' => "SULLY",
+            'other_names' => "ANDREA",
+            'email' => "andrea@gmail.com",
+            'country' => "colombia",
+            'date_of_entry' => $currentDateString,
+            'status' => "Active",
+            'type_of_identity_document_id' => $type_of_identity_document->id,
+            'area_id' => $area->id,
+        ];
+
+        $response = $this->postJson('/api/clients', $body);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonStructure(
+            [
+                'message',
+                'errors' => [
+                    'first_last_name',
+                ]
+            ]
+        );
+
     }
 
     public function testShowSpecificClient(): void
@@ -166,6 +232,94 @@ class ClientApiTest extends TestCase
             'identity_document' => '16450360',
             'first_last_name' => 'MENESES',
         ]);
+    }
+
+    public function testUpdateWithEmptyFields(): void
+    {
+        $client = Client::factory()->create(
+            [
+                'type_of_identity_document_id' => TypeOfIdentityDocument::factory()->create()->id,
+                'area_id' => Area::factory()->create()->id
+            ]
+        );
+
+        $type_of_identity_document = TypeOfIdentityDocument::factory()->create();
+        $area = Area::factory()->create();
+
+        $currentDateString = date('Y-m-d H:i:s');
+        
+        $updateBody = [
+            'identity_document' => "16450360",
+            'first_last_name' => "MENESES",
+            'second_last_name' => "BEJARANO",
+            'first_name' => "SULLY",
+            'other_names' => "ANDREA",
+            'email' => "andrea@gmail.com",
+            'country' => "colombia",
+            'date_of_entry' => $currentDateString,
+            'status' => "Active",
+            'type_of_identity_document_id' => $type_of_identity_document->id,
+            'area_id' => $area->id,
+        ];
+
+        $response = $this->putJson("/api/clients/{$client->id}", []);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonStructure(
+            [
+                'message',
+                'errors' => [
+                    'identity_document',
+                    'first_last_name',
+                    'second_last_name',
+                    'first_name',
+                    'other_names',
+                ]
+            ]
+        );
+    }
+
+    public function testUpdateWithWrongFormat(): void
+    {
+        $client = Client::factory()->create(
+            [
+                'type_of_identity_document_id' => TypeOfIdentityDocument::factory()->create()->id,
+                'area_id' => Area::factory()->create()->id
+            ]
+        );
+
+        $type_of_identity_document = TypeOfIdentityDocument::factory()->create();
+        $area = Area::factory()->create();
+
+        $currentDateString = date('Y-m-d H:i:s');
+        
+        $updateBody = [
+            'identity_document' => "16450360",
+            'first_last_name' => "meneses",
+            'second_last_name' => "BEJARANO",
+            'first_name' => "SULLY",
+            'other_names' => "ANDREA",
+            'email' => "andrea@gmail.com",
+            'country' => "colombia",
+            'date_of_entry' => $currentDateString,
+            'status' => "Active",
+            'type_of_identity_document_id' => $type_of_identity_document->id,
+            'area_id' => $area->id,
+        ];
+
+        $response = $this->putJson("/api/clients/{$client->id}", $updateBody);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonStructure(
+            [
+                'message',
+                'errors' => [
+                    'first_last_name',
+                ]
+            ]
+        );
     }
 
     public function testDestroy(): void
