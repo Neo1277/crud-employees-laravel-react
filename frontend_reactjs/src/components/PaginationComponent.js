18b0a1d589bcd28f4bd12ff4efcd1f04e9eae07f
Marlon Meneses <marlon-armando@hotmail.com>
2026-01-24 16:46:41 -0500

    PaginationComponent fixed

--- frontend_reactjs/src/components/PaginationComponent.js
+++ frontend_reactjs/src/components/PaginationComponent.js
@@ -8,43 +8,36 @@ import {
 export default function PaginationComponent(props) {
     const handleSubmit = (e) => {
         e.preventDefault();
-        const url = e.target.href;
+        const url = e.currentTarget.href;
+        if (!url) return;
         props.fetchClients(url);
     };
     
     return (
         <Container>
             <Pagination aria-label="clients-pagination">
-                <PaginationItem disabled={
-                    props.clients.clients.links.first === null ? true : false
-                }>
+                <PaginationItem disabled={!props.clients.clients.links.first}>
                     <PaginationLink
                         first
                         href={props.clients.clients.links.first}
                         onClick={handleSubmit}
                     />
                 </PaginationItem>
-                <PaginationItem disabled={
-                    props.clients.clients.links.prev === null ? true : false
-                }>
+                <PaginationItem disabled={!props.clients.clients.links.prev}>
                     <PaginationLink
                         href={props.clients.clients.links.prev}
                         previous
                         onClick={handleSubmit}
                     />
                 </PaginationItem>
-                <PaginationItem disabled={
-                    props.clients.clients.links.next === null ? true : false
-                }>
+                <PaginationItem disabled={!props.clients.clients.links.next}>
                     <PaginationLink
                         href={props.clients.clients.links.next}
                         next
                         onClick={handleSubmit}
                     />
                 </PaginationItem>
-                <PaginationItem disabled={
-                    props.clients.clients.links.last === null ? true : false
-                }>
+                <PaginationItem disabled={!props.clients.clients.links.last}>
                     <PaginationLink
                         href={props.clients.clients.links.last}
                         last
